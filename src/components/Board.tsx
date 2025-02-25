cc72a9408c345340b4332ef8a4af4731645d6c48
jasmina <jasmina.jamakovic@gmail.com>
2025-02-25 17:20:42 +0100

    added type safety

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,12 +1,14 @@
 import Cell from './Cell'
+import { TBoard } from '../types'
+const BOARD: TBoard = [
+    [{ value: 0}, { value: 1 }, { value: 2 }, { value: "mine" }],
+    [{ value: 3 }, { value: 4, isOpened: false, isFlagged: false }, { value: 5 }, { value: 6}],
+    [{ value: 7 }, { value: 8 }, { value: "mine" }, { value: "mine" }], 
+]
 const Board = () => {
   return (
     <div className="board">
-    {[
-    [{ value: 0}, { value: 1 }, { value: 2 }, { value: "mine" }],
-    [{ value: 3 }, { value: 4, isOpened: false, isFlagged: true }, { value: 5 }, { value: 6}],
-    [{ value: 7 }, { value: 8 }, { value: "mine" }, { value: "mine" }], 
-    ].map((row) => (
+    {BOARD.map((row) => (
     <div className="flex"> {row.map((cell) => (
     <Cell cell={cell} />
     ))}
---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+type OpenedCell = {
+    isOpened: true;
+    isFlagged: false;
+}
+type ClosedCell = {
+    isOpened: false;
+    isFlagged: boolean;
+}
+type MineCell = {
+    value: 'mine';
+    highlight?: 'red' | 'green';
+}
+type NumberCell = {
+    value: number;
+}
+type EmptyCell = {
+    value: null;
+    isFlagged: false;
+    isOpened: false;
+}
+
+export type OpenedMineCell = OpenedCell & MineCell;
+type ClosedMineCell = ClosedCell & MineCell;
+export type OpenedNumberCell = OpenedCell & NumberCell;
+type ClosedNumberCell = ClosedCell & NumberCell;
+
+export  type GameCell = OpenedMineCell | ClosedMineCell | OpenedNumberCell | ClosedNumberCell | EmptyCell;
+
+export type TBoard = GameCell[][];
